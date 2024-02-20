f4635619dc06971f03dd828a4d664c31ce6ce5af
adams miracle <adamsmiracle0@gmail.com>
2024-02-20 23:13:16 +0000

    task 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,5 @@
+--  prepares a MySQL server for the project:
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
+GRANT SELECT PRIVILEGE ON performance_schema . * TO 'hbnb_test'@'localhost';
