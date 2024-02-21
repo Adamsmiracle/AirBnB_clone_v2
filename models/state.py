a31e220ac29d08d15e3eeae0a9f301234a38e8b5
adams miracle <adamsmiracle0@gmail.com>
2024-02-21 14:43:49 +0000

    task 6

--- models/state.py
+++ models/state.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 import models
 from models.city import City
-from models.base_model import BaseModel, Base
 import shlex
 
 
-class State(BaseModel):
-    """ State class """
+class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    citie = relationship("City", cascade="all, delete, delete-orphan",
-            backref='state')
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
     @property
-    def citie(self):
+    def cities(self):
         var = models.storage.all()
-
         lista = []
-
-        resuls = []
-
-        for city in var:
-            city = city.replace(".", " ")
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
             city = shlex.split(city)
-            if (city[0] == "City"):
+            if (city[0] == 'City'):
                 lista.append(var[key])
-        for element in lista:
-            if (element.id == self.id):
-                results.append(element)
-        return (results)
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
--- models/user.py
+++ models/user.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
+class User(BaseModel, Base):
+    """This is the attributes for user
+
+    """
     email = ''
     password = ''
     first_name = ''
