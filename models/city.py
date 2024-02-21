66c2a437f592109e8531415d62a1d8c3c30773c8
adams miracle <adamsmiracle0@gmail.com>
2024-02-21 14:19:42 +0000

    task 6

--- models/city.py
+++ models/city.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import foreignKey
+from sqlalchemy.0rm import relationship
+from models.place import Place
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'City'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), nullable=False,
+            foreignKey("states.id"))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+            backref="cities")
+    
--- models/state.py
+++ models/state.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+import models
+from models.city import City
+from models.base_model import BaseModel, Base
+import shlex
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    citie = relationship("City", cascade="all, delete, delete-orphan",
+            backref='state')
+
+    @property
+    def citie(self):
+        var = models.storage.all()
+
+        lista = []
+
+        resuls = []
+
+        for city in var:
+            city = city.replace(".", " ")
+            city = shlex.split(city)
+            if (city[0] == "City"):
+                lista.append(var[key])
+        for element in lista:
+            if (element.id == self.id):
+                results.append(element)
+        return (results)
