8d4108882378d24999aa4ae6061c371711be5dd2
adams miracle <adamsmiracle0@gmail.com>
2024-02-21 15:03:01 +0000

    task 6

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -54,38 +54,6 @@ class DBStorage:
                     dic[key] = elem
         return (dic)
 
-#!/usr/bin/python3
-from os import getenv
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import (create_engine)
-from sqlalchemy.declarative import declarative_base
-from models.base_model import Base
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-from models.user import User
-from models.place import Place
-
-
-class DBStorage():
-    __engine = NONE
-    __session = NONE
-
-    def __init__(self):
-        user = getenv("HBNB_MYSQL_USER")
-        passwd = getenv("HBNB_MYSQL_PWD")
-        db = getenv("HBNB_MYSQL_DB")
-        host = getenv("HBNB_MYSQL_HOST")
-        env = getenv("HBNB_ENV")
-
-        self.__engine = create_engine('mysql+mysqldb://{}.{}@{}/{}'
-                                      .format(user, passwd, host, db), pool_pre_ping=True)
-
-        if env == 'test':
-            Base.metadata.drop_all(self.__engine)
-
-    def all(self, cls=NONE):
     def new(self, obj):
         """add a new element in the table
         """
