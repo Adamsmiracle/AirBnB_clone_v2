bb4235262181baaed206cf39ab8a0331c60c36af
adams miracle <adamsmiracle0@gmail.com>
2024-03-07 19:51:44 +0000

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,9 +8,8 @@ from datetime import datetime
 def do_pack():
     """Create a tar gzipped archive"""
     date = datetime.utcnow()
-    dates = (date.year, date.month, date.day,
-             date.hour, date.minute, date.second)
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dates)
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(date.year, date.month,
+                                                         date.day, date.hour, date.minute, date.second)
     if os.path.isdir("versions") is False:
         if local("mkdir -p versions").failed is True:
             return None
